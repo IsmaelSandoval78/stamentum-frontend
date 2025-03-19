d7c028722d20ab9511dab2c0ee052b300e0547ab
Ismael Sandoval <68979245+IsmaelSandoval78@users.noreply.github.com>
2025-03-19 00:19:15 -0400

    Add files via upload

---
+++ pages/adhd.js
@@ -0,0 +1,13 @@
+import { useTranslation } from 'react-i18next';
+
+export default function ADHD() {
+  const { t } = useTranslation();
+
+  return (
+    <div style={ textAlign: 'center', marginTop: '50px' }>
+      <h1>{t('adhd')}</h1>
+      <p>{t('adhdDesc')}</p>
+      <h3>{t('comingSoon')}</h3>
+    </div>
+  );
+}
